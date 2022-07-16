0ccd6ca5c74014eee0cc8665b36531cde9c9ccc0
Swarnnika Raj Singh <swarnikarajsingh@gmail.com>
2022-07-16 16:36:19 +0530

    tttuugu

--- RedisAssign/src/server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./index");
-const connect = require("./configs/db");
-
-app.listen(2345, async function () {
-  await connect();
-  console.log("listening on port 2345");
-});
--- codingEval2/src/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./index");
-
-const connect = require("./configs/db");
-
-app.listen(2345, async function() {
-    await connect();
-    console.log("Listening on port : 2345");
-})
